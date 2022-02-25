14a59128ebf26706a32f1cf9cdd469701c9e076f
14likhit <jaijalaram@4495BCU>
2022-02-25 21:18:16 +0530

    - Added service and foreground permission in Manifest. - Added Timer Service Logic in Main Activit.

--- app/src/main/java/com/test/mytimer/MainActivity.kt
+++ app/src/main/java/com/test/mytimer/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.test.mytimer
 
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.test.mytimer.databinding.ActivityMainBinding
+import com.test.mytimer.service.TimerNotification
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var activityMainBinding: ActivityMainBinding
 
-    val start = 100_000L
+    val start = 600_00L
     var timer = start
     lateinit var countDownTimer: CountDownTimer
 
+    lateinit var notiTimer: TimerNotification.Builder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,14 +39,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun setClickListeners() {
         activityMainBinding.timerInputLayout.startBtn.setOnClickListener {
-            //Todo-> Uncomment Once Service Logic Done.
-//            timer = activityMainBinding.timerInputLayout.timerInputEtv.text.toString().toLong()
-            startTimer()
             setTimerView()
+            startTimerService()
         }
 
         activityMainBinding.timerCountdownLayout.exitBtn.setOnClickListener {
-            exitTimer()
+            exitTimerService()
             setTimerInputView()
         }
     }
@@ -52,16 +55,40 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding.timerInputLayout.timerInputEtv.setText(timer.toString())
     }
 
+    private fun startTimerService() {
+        notiTimer = TimerNotification.Builder(this)
+            .setSmallIcon(R.drawable.ic_timer)
+            .setColor(R.color.purple_200)
+            .setShowWhen(false)
+            .setAutoCancel(false)
+            .setOnlyAlertOnce(true)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setContentIntent(getPendingIntent())
+            .setOnTickListener {
+                activityMainBinding.timerCountdownLayout.countdownTv.text = it.toString()
+            }
+            .setOnFinishListener {
+                Toast.makeText(this, "timer finished", Toast.LENGTH_SHORT).show()
+            }
+            .setContentTitle("Timer :)")
+
+        notiTimer.play(start)
+    }
+
+    private fun exitTimerService() {
+        notiTimer.terminate()
+    }
+
     private fun setTimerView() {
         activityMainBinding.timerInputLayout.timerInputRootCl.visibility = View.GONE
         activityMainBinding.timerCountdownLayout.timerCountdownRootCl.visibility = View.VISIBLE
     }
 
-    private fun startTimer(){
-        countDownTimer = object : CountDownTimer(timer,1000){
+    private fun startTimer() {
+        countDownTimer = object : CountDownTimer(timer, 1000) {
             //            end of timer
             override fun onFinish() {
-                Toast.makeText(this@MainActivity,"end timer",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "end timer", Toast.LENGTH_SHORT).show()
             }
 
             override fun onTick(millisUntilFinished: Long) {
@@ -85,5 +112,12 @@ class MainActivity : AppCompatActivity() {
         val format = String.format("%02d:%02d", m, s)
 
         activityMainBinding.timerCountdownLayout.countdownTv.text = format
+    }
+
+    private fun getPendingIntent(): PendingIntent {
+        return Intent(this, MainActivity::class.java).let {
+            PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
     }
 }
