fce50c3c3bd219b02295f888e2ab23d2979434ad
14likhit <jaijalaram@4495BCU>
2022-02-25 20:31:33 +0530

    - Added Timer Logic.

--- app/src/main/java/com/test/mytimer/MainActivity.kt
+++ app/src/main/java/com/test/mytimer/MainActivity.kt
@@ -2,12 +2,19 @@ package com.test.mytimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
+import android.widget.Toast
 import com.test.mytimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var activityMainBinding: ActivityMainBinding
+
+    val start = 100_000L
+    var timer = start
+    lateinit var countDownTimer: CountDownTimer
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,14 +39,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         activityMainBinding.timerCountdownLayout.exitBtn.setOnClickListener {
+            exitTimer()
             setTimerInputView()
-
         }
     }
 
     private fun setTimerInputView() {
         activityMainBinding.timerCountdownLayout.timerCountdownRootCl.visibility = View.GONE
         activityMainBinding.timerInputLayout.timerInputRootCl.visibility = View.VISIBLE
+        activityMainBinding.timerInputLayout.timerInputEtv.setText(timer.toString())
     }
 
     private fun setTimerView() {
@@ -48,10 +56,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startTimer(){
-        val time = activityMainBinding.timerInputLayout.timerInputEtv.text
+        countDownTimer = object : CountDownTimer(timer,1000){
+            //            end of timer
+            override fun onFinish() {
+                Toast.makeText(this@MainActivity,"end timer",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                timer = millisUntilFinished
+                setTextTimer()
+            }
+
+        }.start()
     }
 
     private fun exitTimer(){
+        countDownTimer.cancel()
+        timer = start
+        setTextTimer()
+    }
 
+    fun setTextTimer() {
+        val m = (timer / 1000) / 60
+        val s = (timer / 1000) % 60
+
+        val format = String.format("%02d:%02d", m, s)
+
+        activityMainBinding.timerCountdownLayout.countdownTv.text = format
     }
 }
