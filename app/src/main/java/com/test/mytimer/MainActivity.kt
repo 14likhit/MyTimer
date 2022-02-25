a1af5e01fa6d952c191e15f4c52db04671ab2f6a
14likhit <jaijalaram@4495BCU>
2022-02-25 20:24:42 +0530

    - Added Template logic for inputs.

--- app/src/main/java/com/test/mytimer/MainActivity.kt
+++ app/src/main/java/com/test/mytimer/MainActivity.kt
@@ -2,10 +2,56 @@ package com.test.mytimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.test.mytimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        init()
+    }
+
+    private fun init() {
+
+        setClickListeners()
+
+        setTimerInputView()
+
+    }
+
+    private fun setClickListeners() {
+        activityMainBinding.timerInputLayout.startBtn.setOnClickListener {
+            startTimer()
+            setTimerView()
+        }
+
+        activityMainBinding.timerCountdownLayout.exitBtn.setOnClickListener {
+            setTimerInputView()
+
+        }
+    }
+
+    private fun setTimerInputView() {
+        activityMainBinding.timerCountdownLayout.timerCountdownRootCl.visibility = View.GONE
+        activityMainBinding.timerInputLayout.timerInputRootCl.visibility = View.VISIBLE
+    }
+
+    private fun setTimerView() {
+        activityMainBinding.timerInputLayout.timerInputRootCl.visibility = View.GONE
+        activityMainBinding.timerCountdownLayout.timerCountdownRootCl.visibility = View.VISIBLE
+    }
+
+    private fun startTimer(){
+        val time = activityMainBinding.timerInputLayout.timerInputEtv.text
+    }
+
+    private fun exitTimer(){
+
     }
 }
